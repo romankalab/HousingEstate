55c24318596cd82dc124dde0e3979fc46b0c29a2
romankalab <roan.kalab@gmail.com>
2019-12-12 09:07:51 +0000

    explanation

--- HousingEstate/Program.cs
+++ HousingEstate/Program.cs
@@ -16,8 +16,52 @@ namespace HousingEstate
             flat1.People.Add(person1);
             flat1.People.Add(person2);
             entrance1.FlatList.Add(flat1);
-            blockOfFlats1.EntranceList.Add(entrance1);
+            //blockOfFlats1.EntranceList.Add(entrance1);
             housingEstate.BlockOfFlats.Add(blockOfFlats1);
+
+
+
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    blockOfFlats1.EntranceList.Add(new Entrance(i * 2));
+            //}
+
+            //foreach (var entrance in blockOfFlats1.EntranceList)
+            //{
+            //    for (int i = 1; i < 15; i++)
+            //    {
+            //        entrance.FlatList.Add(new Flat(i));
+            //    }
+            //}
+
+            //foreach (var entrance in blockOfFlats1.EntranceList)
+            //{
+            //    foreach (var flat in entrance.FlatList)
+            //    {
+            //        for (int i = 0; i < GetRandomNumberOfPeople(); i++)
+            //        {
+            //            flat.People.Add(new Person(GetRandomName(), GetRandomSurname, GetRandomAge()));
+            //        }
+            //    }
+            //}
+
+            
+            Console.WriteLine(GetRandomName());
+
+        }
+
+        private static string GetRandomName()
+        {
+            List<string> randomNames = new List<string>
+                {
+                    "Roman",
+                    "Jakub",
+                    "Martin"
+                };
+
+            Random rand = new Random();
+
+            return randomNames.ToArray()[rand.Next(0, randomNames.Count)];
         }
     }
 }
